c03fd96643a830be26fa089e80bacfdccf88813a
ya-web <idir.alliche@gmail.com>
2024-10-22 19:44:56 +0200

    Create homepage

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenue aux Jeux Olympiques 2024</h1>
+      <p>Les Jeux Olympiques de 2024 se dérouleront à Paris.</p>
+      
+      <h2>Épreuves</h2>
+      <ul>
+        <li>100m - Athlétisme - Stade Olympique - 26 juillet</li>
+        <li>200m - Athlétisme - Stade Olympique - 27 juillet</li>
+        <li>Relais 4x100m - Athlétisme - Stade Olympique - 28 juillet</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
