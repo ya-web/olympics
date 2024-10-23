ffd3c6dc0a0ed2482c4250b785ff94956efd5623
ya-web <idir.alliche@gmail.com>
2024-10-23 11:21:10 +0200

    Add some customizations and style on Events to Homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -9,35 +9,53 @@ const Home = () => {
       <Header />
       <h1>Bienvenue aux Jeux Olympiques 2024</h1>
       <p className='home_desc'>Les Jeux Olympiques de 2024 se dérouleront à Paris. Venez vivre des moments historiques en assistant aux plus grandes compétitions sportives mondiales.</p>
-      
-      <h2>Les Épreuves</h2>
+
       <div className="epreuves">
+        <h2>Les Épreuves</h2>
+
+
         <div className="epreuve">
           <h3><FaRunning /> Athlétisme - 100m</h3>
-          <p><strong>Date :</strong> 26 juillet 2024</p>
-          <p><strong>Lieu :</strong> Stade Olympique</p>
-          <button>Réservez vos places</button>
+          <div className="details">
+            <span><strong>Date :</strong> 26 juillet 2024</span>
+            <span><strong>Lieu :</strong> Stade Olympique</span>
+          </div>
+          <div className="description">
+            Série éliminatoire messieurs, groupe 1
+          </div>
+          <button>Réserver</button>
         </div>
 
         <div className="epreuve">
           <h3><FaRunning /> Athlétisme - 200m</h3>
-          <p><strong>Date :</strong> 27 juillet 2024</p>
-          <p><strong>Lieu :</strong> Stade Olympique</p>
-          <button>Réservez vos places</button>
+          <div className="details">
+            <span><strong>Date :</strong> 27 juillet 2024</span>
+            <span><strong>Lieu :</strong> Stade Olympique</span>
+          </div>
+          <div className="description">
+            Demi-finale dames, groupe 1
+          </div>
+          <button>Réserver</button>
         </div>
 
         <div className="epreuve">
           <h3><FaRunning /> Relais 4x100m</h3>
-          <p><strong>Date :</strong> 28 juillet 2024</p>
-          <p><strong>Lieu :</strong> Stade Olympique</p>
-          <button>Réservez vos places</button>
+          <div className="details">
+            <span><strong>Date :</strong> 28 juillet 2024</span>
+            <span><strong>Lieu :</strong> Stade Olympique</span>
+          </div>
+          <div className="description">
+            Finale hommes
+          </div>
+          <button>Réserver</button>
         </div>
-      </div>
 
-      <div className="view-all">
-        <Link to="/epreuves">
-          <button>Voir toutes les épreuves</button>
-        </Link>
+        <div className="view-all">
+          <Link to="/epreuves">
+            <button>Voir toutes les épreuves</button>
+          </Link>
+        </div>
+
       </div>
 
       <Footer />
