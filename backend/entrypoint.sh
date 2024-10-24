be074b9f33f2b2491c2a5099ab7c24e2762b1a93
ya-web <idir.alliche@gmail.com>
2024-10-24 17:05:50 +0200

    Add automation for django to serve frontend build from react (simulates cdn in dev mode)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# read secrets from environment variables
+# Read secrets from environment variables
 export POSTGRES_USER=$(cat /run/secrets/postgres_user)
 export POSTGRES_PASSWORD=$(cat /run/secrets/postgres_password)
 export POSTGRES_DB=$(cat /run/secrets/postgres_db)
 
-# Start PostgreSQL with environment variables
+# Check if the service is Django (optional)
+if [ "$SERVICE" = "django" ]; then
+    # Collect static files
+    echo "Collecting static files..."
+    python manage.py collectstatic --noinput
+
+    # Apply database migrations (optional)
+    echo "Applying database migrations..."
+    python manage.py migrate
+fi
+
+# Start the main process (PostgreSQL or Django server depending on the service)
 exec docker-entrypoint.sh postgres
+
